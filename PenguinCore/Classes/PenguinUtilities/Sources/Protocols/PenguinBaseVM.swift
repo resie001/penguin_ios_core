c6565a27f404c6e2d99de9da707d6989854e736b
Ade Resie <chevalier@resie.id>
2024-07-25 16:41:17 +0700

    Penguin Base VM

---
+++ PenguinCore/Classes/PenguinUtilities/Sources/Protocols/PenguinBaseVM.swift
@@ -0,0 +1,15 @@
+//
+//  PenguinBaseVM.swift
+//  PenguinUtilities
+//
+//  Created by Ade Resie on 25/07/24.
+//
+
+import Foundation
+
+public protocol PenguinBaseVM {
+    associatedtype Input
+    associatedtype Output
+    
+    func transform(input: Input) -> Output
+}
